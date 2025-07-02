9434a07ef4ac1c03e262e5c275dbcb2b07a7a5fb
Khai <khaiaw@stanford.edu>
2025-07-02 13:21:50 -0700

    update plot loss

--- plot_loss.py
+++ plot_loss.py
@@ -1,31 +1,40 @@
 import os
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 n_iters_per_epoch = 300
-log_dir = 'pretrain/16.8.vitl.256px.16f/lr_default'
-dfs = []
+bucket_size = 10
+log_dir = 'pretrain/16.8.vitl.256px.16f/kinetics_default_lr'
+loss_list_for_each_logfile = []
 for r in range(8):
     file_path = os.path.join(log_dir, f'log_r{r}.csv')
-    dfs.append(pd.read_csv(file_path))
-log_df = pd.concat(dfs, ignore_index=True)
+    log_df = pd.read_csv(file_path)
+    log_df = log_df[~log_df['epoch'].astype(str).str.startswith('epoch')]
+    max_epoch = log_df['epoch'].max()
+    loss_list = pd.to_numeric(log_df['loss'], errors='coerce').tolist()  # Convert 'loss' column to a numeric list
+    loss_list_for_each_logfile.append(loss_list)
+    
+# Average the losses across all logfiles
 
-log_df = log_df[~log_df['epoch'].astype(str).str.startswith('epoch')]
-max_epoch = log_df['epoch'].max()
+loss_list_averaged = []
+min_len = min(len(l) for l in loss_list_for_each_logfile)
+for i in range(min_len):
+    avg_loss = sum(log[i] for log in loss_list_for_each_logfile) / len(loss_list_for_each_logfile)
+    loss_list_averaged.append(avg_loss)
 
-log_df['overall_iter'] = log_df['epoch'] * n_iters_per_epoch + log_df['itr']
-log_df['loss'] = pd.to_numeric(log_df['loss'], errors='coerce') # Ensure 'loss' is numeric before groupby to avoid aggregation errors
-log_df['epoch'] = pd.to_numeric(log_df['epoch'], errors='coerce')  # Ensure 'epoch' is numeric
-mean_loss_per_epoch = log_df.groupby('epoch', as_index=False)['loss'].mean()
+# group into buckets of bucket_size and average
+bucket_losses = []
+for i in range(0, len(loss_list_averaged), bucket_size):
+    bucket = loss_list_averaged[i:i + bucket_size]
+    bucket_losses.append(np.mean(bucket))
 
-plt.figure(figsize=(40, 5))
-plt.plot(mean_loss_per_epoch['epoch'], mean_loss_per_epoch['loss'], marker='o', label='Mean Loss per Epoch')
-plt.title('Mean Loss per Epoch')
-plt.xlabel('Epoch')
+plt.plot(bucket_losses, label='Mean Loss')
+plt.xticks(range(0, len(bucket_losses), n_iters_per_epoch // bucket_size), range(0, max_epoch))
 plt.ylabel('Mean Loss')
-plt.xticks(mean_loss_per_epoch['epoch'])
-plt.grid()
+plt.title('Loss per Bucket')
 plt.legend()
+
 plt.tight_layout()
 plt.savefig(os.path.join(log_dir, f'mean_loss_per_epoch_max_{max_epoch}.png'))
 plt.show()
